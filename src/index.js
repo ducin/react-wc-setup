d127abfac059017bd06961992c68e71081be2831
Tomasz Ducin <tomasz.ducin@gmail.com>
2017-12-15 12:03:24 +0100

    initial setup

---
+++ src/index.js
@@ -0,0 +1,14 @@
+// custom elements wrapper for ES5 (only ES6+ works without it)
+import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter'
+
+// all components below
+import './components/my-hello/my-hello';
+// web components are used from HTML level
+
+// this single react component is rendered without webcomponents
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Hello from './components/my-hello/Hello';
+
+const $outside = document.getElementById("outside");
+ReactDOM.render(<Hello />, $outside);
---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+const path = require('path');
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
+  template: './src/index.html',
+  filename: 'index.html',
+  inject: 'body'
+});
+
+const devServerPort = 8083;
+
+module.exports = {
+  entry: {
+    hmr: [
+      'webpack-dev-server/client?http://localhost:' + devServerPort,
+    ],
+    bundle: "./src/index.js"
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: '[name].js'
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+      { test: /\.jsx?$/, loader: 'babel-loader', exclude: /node_modules/ },
+    ],
+  },
+  plugins: [
+    HtmlWebpackPluginConfig
+  ],
+  devServer: {
+    port: devServerPort,
+    inline: true
+  }
+}
