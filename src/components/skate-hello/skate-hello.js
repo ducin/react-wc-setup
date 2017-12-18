29a006a52d5a50e4835fb9fed9845a6e09deaed2
Piotr Gajda <piotr.gajda@ivanti.com>
2017-12-18 17:38:13 +0100

    kot

--- src/components/skate-hello/skate-hello.js
+++ src/components/skate-hello/skate-hello.js
@@ -1,10 +1,22 @@
-// @jsx React.createElement
-
 import { props, withComponent } from 'skatejs';
 import withReact from '@skatejs/renderer-react';
 import React from 'react';
+import Hello from '../my-hello/Hello';
 
-class SkateHello extends withComponent(withReact()) {
+
+
+class SkateHello extends withComponent() {
+  static get props() {
+    return {
+      name: props.string
+    };
+  }
+  render({ name }) {
+    return `Hello, ${name}!`;
+  }
+}
+
+class SkateHelloReact extends withComponent(withReact()) {
   static get props() {
     return {
       name: props.string
@@ -15,4 +27,17 @@ class SkateHello extends withComponent(withReact()) {
   }
 }
 
+class ReactAsSkateHello extends withReact(withComponent()) {
+  render( ) {
+    return (
+      <div>
+        <span>elo mordo</span>
+        <Hello />
+      </div>
+    );
+  }
+}
+
 customElements.define('skate-hello', SkateHello);
+customElements.define('skate-hello-react', SkateHelloReact);
+customElements.define('react-as-skate-hello', ReactAsSkateHello);
--- src/index.js
+++ src/index.js
@@ -1,18 +1,10 @@
-<<<<<<< HEAD
-// custom elements wrapper for ES5
-// import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter'
-
-// all components below
-import './components/my-hello/my-hello';
-import './components/skate-hello/skate-hello';
-=======
 // custom elements wrapper for ES5 (only ES6+ works without it)
 import '@webcomponents/webcomponentsjs/custom-elements-es5-adapter'
 
 // all components below
 import './components/my-hello/my-hello';
 // web components are used from HTML level
-
+import './components/skate-hello/skate-hello';
 // this single react component is rendered without webcomponents
 import React from 'react';
 import ReactDOM from 'react-dom';
@@ -20,4 +12,3 @@ import Hello from './components/my-hello/Hello';
 
 const $outside = document.getElementById("outside");
 ReactDOM.render(<Hello />, $outside);
->>>>>>> d127abfac059017bd06961992c68e71081be2831
