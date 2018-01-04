1ab6256c81235d70519447bf7bcc5ef370d8adc0
Tomasz Ducin <tomasz.ducin@gmail.com>
2018-01-04 10:22:27 +0100

    Revert "jp banda"

--- src/index.js
+++ src/index.js
@@ -9,4 +9,3 @@ import './components/my-hello/skate-hello-wrapper';
 // web components are used from HTML level
 
 import './app';
-import './app.scss';
--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
 
 const devServerPort = 8083;
 
@@ -13,38 +12,16 @@ module.exports = {
     filename: '[name].[hash:8].js'
   },
   module: {
-    rules: [{
-        test: /\.js$/,
-        use: [
-          'babel-loader'
-        ]
-      },
-      {
-        test: /\.jsx$/,
-        use: [
-          'babel-loader'
-        ]
-      },
-      {
-        test: /\.scss$/,
-        use: ExtractTextPlugin.extract({
-          fallback: 'style-loader',
-          use: ['css-loader', 'sass-loader'],
-          publicPath: path.resolve(__dirname, "dist")
-        })
-      }
-    ]
+    loaders: [
+      { test: /\.js$/, loader: 'babel-loader', /* exclude: /node_modules/ */ },
+      { test: /\.jsx?$/, loader: 'babel-loader', /* exclude: /node_modules/ */ },
+    ],
   },
   plugins: [
     new HtmlWebpackPlugin({
       template: './src/index.html',
       filename: 'index.html',
       inject: 'body'
-    }),
-    new ExtractTextPlugin({
-      filename: 'app.css',
-      disable: false,
-      allChunks: true
     })
   ]
 }
