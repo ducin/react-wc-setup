eed6371b4d109ce5af8e1139e4ccc0177d750861
Piotr Gajda <piotr.gajda@ivanti.com>
2018-01-08 16:56:45 +0100

    w twoja twarz

--- src/components/my-hello/MyHello.js
+++ src/components/my-hello/MyHello.js
@@ -1,14 +1,24 @@
 import React, { Component } from 'react';
-// import logo from '../logo.svg';
-// import './MyHello.css';
+import styled from 'styled-components';
+const StyledButton = styled.button`
+  background-color: red
+`;
+
+const inlineStyle = {
+  backgroundColor: 'pink'
+};
+
 
 class MyHello extends Component {
   article(phrase){
     return ['a', 'e', 'o', 'i', 'u', 'y', 'h'].includes(phrase[0].toLowerCase()) ? 'an' : 'a';
   }
+
   render() {
     return (
       <div className="MyHello test-me">
+        <StyledButton> styled components button (red)</StyledButton>
+        <div style={inlineStyle}>inline styled div (pink)</div>
         <header className="MyHello-header">
           <img src={"https://picsum.photos/100/100?" + Math.random()} className="MyHello-logo" alt="logo" />
           <h2 className="MyHello-title">I'm {this.article(this.props.name)} {this.props.name} component!</h2>
@@ -20,5 +30,7 @@ class MyHello extends Component {
     );
   }
 }
+
+
 
 export default MyHello;
